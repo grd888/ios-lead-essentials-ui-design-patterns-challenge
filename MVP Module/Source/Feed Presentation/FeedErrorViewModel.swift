6fad4bdc737af3a38dba929f04a00cb13cde703a
Greg Delgado <grd888@gmail.com>
2021-09-13 07:38:35 +0800

    create FeedErrorViewModel to communicate error messages to the Error view

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedErrorViewModel.swift
+//  MVP
+//
+//  Created by GD on 9/13/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+struct FeedErrorViewModel {
+	let errorMessage: String?
+}
