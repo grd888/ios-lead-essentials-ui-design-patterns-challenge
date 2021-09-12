cf895fb6cf8e47e7e7d0ae64a179017a92c691fa
Greg Delgado <grd888@gmail.com>
2021-09-12 20:01:15 +0800

    connect ErrorView IBOutlet

--- MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
+++ MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
@@ -5,6 +5,8 @@
 import UIKit
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
+	@IBOutlet private(set) public var errorView: ErrorView!
+
 	var viewModel: FeedViewModel? {
 		didSet { bind() }
 	}
@@ -30,6 +32,13 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 				self?.refreshControl?.beginRefreshing()
 			} else {
 				self?.refreshControl?.endRefreshing()
+			}
+		}
+		viewModel?.onErrorMessage = { [weak self] errorMessage in
+			if let errorMessage = errorMessage {
+				self?.errorView.show(message: errorMessage)
+			} else {
+				self?.errorView.hideMessage()
 			}
 		}
 	}
